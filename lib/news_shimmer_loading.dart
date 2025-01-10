54e709fad6029f3266650d1309f225d91b2cf240
punkushal <spycymagar@gmail.com>
2025-01-10 15:46:35 +0545

    initial files are added

---
+++ lib/news_shimmer_loading.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class NewsShimmerLoading extends StatelessWidget {
+  const NewsShimmerLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade300,
+      highlightColor: Colors.grey.shade100,
+      child: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Top stories text based
+              Container(
+                height: 40,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
