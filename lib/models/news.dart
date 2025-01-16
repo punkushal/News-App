0700ac070ea7228de3f4f7abc06337dc795df10e
punkushal <spycymagar@gmail.com>
2025-01-16 09:50:25 +0545

    news models modified according json

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -1,29 +1,74 @@
 class News {
-  final String title;
-  final String description;
-  //website url
-  final String url;
-  final String urlToImage;
-  final DateTime publishedAt;
-  final String source;
-
   News({
+    required this.status,
+    required this.totalResults,
+    required this.articles,
+  });
+
+  final String? status;
+  final int? totalResults;
+  final List<Article> articles;
+
+  factory News.fromJson(Map<String, dynamic> json) {
+    return News(
+      status: json["status"],
+      totalResults: json["totalResults"],
+      articles: json["articles"] == null
+          ? []
+          : List<Article>.from(
+              json["articles"]!.map((x) => Article.fromJson(x))),
+    );
+  }
+}
+
+class Article {
+  Article({
+    required this.source,
+    required this.author,
     required this.title,
     required this.description,
     required this.url,
     required this.urlToImage,
     required this.publishedAt,
-    required this.source,
+    required this.content,
   });
 
-  factory News.fromJson(Map<String, dynamic> json) {
-    return News(
-      title: json['title'],
-      description: json['description'],
-      url: json['url'],
-      urlToImage: json['urlToImage'],
-      publishedAt: json['publishedAt'],
-      source: json['source']['name'],
+  final Source? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final DateTime? publishedAt;
+  final String? content;
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      source: json["source"] == null ? null : Source.fromJson(json["source"]),
+      author: json["author"] ?? 'no author',
+      title: json["title"],
+      description: json["description"],
+      url: json["url"],
+      urlToImage: json["urlToImage"],
+      publishedAt: DateTime.tryParse(json["publishedAt"] ?? ""),
+      content: json["content"],
+    );
+  }
+}
+
+class Source {
+  Source({
+    required this.id,
+    required this.name,
+  });
+
+  final String? id;
+  final String? name;
+
+  factory Source.fromJson(Map<String, dynamic> json) {
+    return Source(
+      id: json["id"],
+      name: json["name"],
     );
   }
 }
